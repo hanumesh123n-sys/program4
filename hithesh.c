54e457d0475b349bcab71d9e93acb05e4bd10f6b
hanumesh123n-sys <hanumesh123n@gmail.com>
2025-11-11 14:44:34 +0530

    Add files via upload

---
+++ hithesh.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int num1;
+    printf("enter a number :");
+    scanf("%d",&num1);
+
+    if(num1%2==0)
+    {
+        printf("kanakapa");
+    }
+    else
+    {
+        printf("the enterd number is odd");
+    }
+    return 0;
+
+}
